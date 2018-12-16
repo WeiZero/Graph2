67909309c5486eac16757f7a2eb280db0073b0ef
WeiZero <cd232310@gmail.com>
2018-12-16 15:49:12 +0800

    release test

---
+++ x64/Release/Shader/FBO.fs
@@ -0,0 +1,14 @@
+//FBO fragment shader
+#version 430 core
+
+out vec4 FragColor;
+
+in vec2 TexCoords;
+
+uniform sampler2D screenTexture;
+
+void main()
+{
+	
+	FragColor = texture(screenTexture, TexCoords);
+} 
